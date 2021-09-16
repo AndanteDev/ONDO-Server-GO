2964101a677ecbb85d01c084076e8cb3bcd230b1
vinylrich <whktjd0109@gmail.com>
2021-09-16 21:12:08 +0900

    [FEAT] feat state generator

---
+++ utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+func GenerateStateOauthToken(rdb *redis.Client, c *gin.Context) string {
+	return nil
+}
