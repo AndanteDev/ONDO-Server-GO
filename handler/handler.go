a95df79546d45c23b5d5804189944928a9534fe7
vinylrich <whktjd0109@gmail.com>
2021-09-26 15:05:31 +0900

    [FEAT] Identify Os and switch case

--- handler/handler.go
+++ handler/handler.go
@@ -8,14 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type OauthConn interface {
-	loginHandler(c *gin.Context)
-}
-type Oauth struct {
-	oauth OauthConn
-	os    string
-}
-
 func kakaoLoginHandler(c *gin.Context) {
 
 }
@@ -30,10 +22,19 @@ func googleLoginHandler(c *gin.Context) {
 func googleCallBackHandler(c *gin.Context) {
 	oauthstate := rdb.Get(c, "oauthstate")
 	if c.Request.FormValue("state") != oauthstate.Val() {
-		log.Println("invaild google state Token:", oauthstate.Val())
+		log.Printf("invaild google state Token:%s state:%s", oauthstate.Val(), c.Request.FormValue("state"))
 		c.Redirect(http.StatusTemporaryRedirect, "/")
+		return
 	}
+	data, err := utils.GetGoogleUserInfo(c, c.Request.FormValue("code"), googlewebOauthConfig)
+	if err != nil {
+		log.Println(err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, "/")
+		return
+	}
+	c.JSON(http.StatusOK, data)
 }
+
 func kakaoCallBackHandler(c *gin.Context) {
 
 }
