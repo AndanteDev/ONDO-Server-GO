43bb3d49d957c2e00132bdbe3326b555213323ed
vinylrich <whktjd0109@gmail.com>
2021-09-26 15:08:59 +0900

    [FEAT] Seperate OS Oauth

--- handler/handlerfunc.go
+++ handler/handlerfunc.go
@@ -10,16 +10,18 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
+var googlewebOauthConfig = oauth2.Config{
+	RedirectURL:  info.GoogleRedirectPath,
+	ClientID:     os.Getenv("webclient_id"),
+	ClientSecret: os.Getenv("webclient_secret"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+	Endpoint:     google.Endpoint,
+}
+
 func webOauth(c *gin.Context) string {
-	var googleOauthConfig = oauth2.Config{
-		RedirectURL:  info.GoogleRedirectPath,
-		ClientID:     os.Getenv("webclient_id"),
-		ClientSecret: os.Getenv("webclient_secret"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint:     google.Endpoint,
-	}
-	state := utils.GenerateStateOauthToken(rdb, c)
-	url := googleOauthConfig.AuthCodeURL(state)
+
+	accessToken, refreshToken := utils.GenerateStateOauthToken(rdb, c)
+	url := googlewebOauthConfig.AuthCodeURL(state)
 	return url
 }
 
