0ad04203c1516bce38e9e4bf9c061777af3b6805
vinylrich <whktjd0109@gmail.com>
2021-09-30 16:55:24 +0900

    [FEAT] Bind model Token and Config

--- model/model.go
+++ model/model.go
@@ -1,13 +1,27 @@
 package model
 
-type DBHandler interface{
-	
+import "time"
+
+type DBHandler interface {
 }
-type MysqlHandler
-type User struct {
-	Id int `json:"id"`
-	username `json:"password"`
+type MysqlHandler struct {
+}
+type user struct {
+	Id       int    `json:"id"`
+	Mail     string `json:"username"`
+	Password string `json:"password"`
 }
 
-
-func 
+type Token struct {
+	Access_token  string    `json:"access_token"`
+	Token_type    string    `json:"token_type"`
+	Expiration    time.Time `json:"expires_in"`
+	Refresh_token string    `json:"refresh_token"`
+}
+type OauthConfig struct {
+	Code          string
+	Client_id     string
+	Client_secret string
+	Redirect_uri  string
+	Grant_type    string
+}
